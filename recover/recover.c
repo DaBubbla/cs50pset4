61eb77c3f83c53fe81f45bb6a608ba31606ea652
Edward Johnston <>
2018-07-26 16:59:04 +0000

    First commit, framework layout

--- recover/recover.c
+++ recover/recover.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
-#include <cs50.h>
+//#include <cs50.h>
 
-int main(void)
+
+int main(int argc, char *argv[])
 {
-    //Accept ONE command line arg
-    //else fprintf(stderr); return 1;
+    //ENSURE PROPER USAGE - ONE ARGUMENT ONLY!
+    if(argc != 2)
+    {
+        fprintf(stderr, "Usage: ./recover infile\n");
+        return 1;
+    }
+    //DECLARE FILENAME
+    char *infile = argv[1];
+    //char *outfile = argv[2]; in case outfile must be specified...
 
-    //if image cant be opened
-    //fprintf(stderr) return 2;
+    //OPEN INPUT FILE
+    FILE *inptr = fopen(infile, "r");
+    if(inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", infile);
+        return 2;
+    }
 
+    //OPEN CARD FILE
+        //REPEAT UNTIL END OF CARD FILE
+            //READ 512 BYTES INTO A BUFFER
+
+            //START OF A NEW JPEG?
+                //YES?
+                //NO?
+            //ALREAD FOUND A JPEG?
+                //NO?
+                //YES?
+            //CLOSE REMAINING FILES
 
     //Success!
     return 0;
+    fclose(inptr);
+    //fclose(output??);
 }
